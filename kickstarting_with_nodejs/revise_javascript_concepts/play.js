08c049c15a2cc18aafcbbfc2adbc34334d89ecd6
Venu <venukb6@gmail.com>
2024-05-29 11:58:34 +0530

    Write an arrow function which returns the product of two numbers

---
+++ kickstarting_with_nodejs/revise_javascript_concepts/play.js
@@ -0,0 +1,11 @@
+//1. Write an arrow function which returns the product of two numbers
+
+//first method
+// const productOfTwoNumbers = (a,b) =>{
+//     return a*b
+// }
+
+//second method
+const productOfTwoNumbers = (a, b) => a*b
+
+console.log(productOfTwoNumbers(3,2))
