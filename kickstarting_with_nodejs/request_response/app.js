1441b8f70ad42506eae08db2f68963026e743698
Venu <venukb6@gmail.com>
2024-05-30 11:54:39 +0530

    return custom responses based on the url the user hits

--- kickstarting_with_nodejs/request_response/app.js
+++ kickstarting_with_nodejs/request_response/app.js
@@ -1,12 +1,43 @@
 const http = require('http')
 
 const server = http.createServer((req, res)=>{
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>')
-    res.write('<head><title>My First Page</title></head>')
-    res.write('<body><h1>Welcome to my node js project</h1></body>')
-    res.write('</html>')
-    res.end()
+    const url = req.url
+    if(url === '/'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>')
+        res.write('<head><title>My First Page</title></head>')
+        res.write('<body><h1>Welcome to my node js project</h1></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    
+    if(url === '/home'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>')
+        res.write('<head><title>Home Page</title></head>')
+        res.write('<body><h1>Welcome to home page</h1></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    
+    if(url === '/about'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>')
+        res.write('<head><title>About Page</title></head>')
+        res.write('<body><h1>Welcome to about page</h1></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    
+    if(url === '/node'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>')
+        res.write('<head><title>Node Project Page</title></head>')
+        res.write('<body><h1>Welcome to my node js project</h1></body>')
+        res.write('</html>')
+        return res.end()
+    }
+    
 })
 
 server.listen(3000)
