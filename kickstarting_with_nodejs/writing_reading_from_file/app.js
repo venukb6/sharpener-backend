19c8c0dbf0f676aa40dab6bbd8ce294c6265ba3d
Venu <venukb6@gmail.com>
2024-05-30 13:20:31 +0530

    create app that has form and accepts input from user and write it to a new file

---
+++ kickstarting_with_nodejs/writing_reading_from_file/app.js
@@ -0,0 +1,36 @@
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((req, res)=>{
+    const url = req.url
+    const method = req.method
+
+    if (url === '/'){
+        res.write('<html>')
+        res.write('<head><title>Enter Message</title></head>')
+        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>')
+        res.write('</html>')
+        return res.end()
+    }
+
+    if (url === '/message' && method === 'POST'){
+        const body = []
+        req.on('data', (chunck)=>{
+            console.log(chunck)
+            body.push(chunck)
+        })
+        return req.on('end', ()=>{
+            const parsedBody = Buffer.concat(body).toString()
+            const message = parsedBody.split('=')[1]
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302
+                res.setHeader('Location', '/')
+                return res.end()
+            })
+            
+        })
+    }
+})
+
+
+server.listen(3000)
