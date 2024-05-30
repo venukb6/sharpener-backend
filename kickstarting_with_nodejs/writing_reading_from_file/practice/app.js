66f08e338e5cfc7faedbbf4c440a85d70639495c
Venu <venukb6@gmail.com>
2024-05-30 15:42:59 +0530

    cleaned up the code by moving the request handling part of the code to seperate routes file and tried exporting in all 3 ways and checked the output

--- kickstarting_with_nodejs/writing_reading_from_file/practice/app.js
+++ kickstarting_with_nodejs/writing_reading_from_file/practice/app.js
@@ -1,36 +1,9 @@
 const http = require('http')
 const fs = require('fs')
+const routes = require('./routes')
 
-const server = http.createServer((req, res)=>{
-    const url = req.url
-    const method = req.method
-
-    if (url === '/'){
-        res.write('<html>')
-        res.write('<head><title>Enter Message</title></head>')
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"/><button type="submit">Send</button></form></body>')
-        res.write('</html>')
-        return res.end()
-    }
-
-    if (url === '/message' && method === 'POST'){
-        const body = []
-        req.on('data', (chunck)=>{
-            console.log(chunck)
-            body.push(chunck)
-        })
-        return req.on('end', ()=>{
-            const parsedBody = Buffer.concat(body).toString()
-            const message = parsedBody.split('=')[1]
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode = 302
-                res.setHeader('Location', '/')
-                return res.end()
-            })
-            
-        })
-    }
-})
+const server = http.createServer(routes.handler)
+console.log(routes.someText)
 
 
 server.listen(3000)
