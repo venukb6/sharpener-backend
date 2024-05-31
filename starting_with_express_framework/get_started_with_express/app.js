a43685d3afbc2d7281c0f9351c98788ba3a341fd
Venu <venukb6@gmail.com>
2024-05-31 11:50:39 +0530

    added middlewares

--- starting_with_express_framework/get_started_with_express/app.js
+++ starting_with_express_framework/get_started_with_express/app.js
@@ -4,6 +4,19 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next)=>{
+    console.log('In the middleware')
+    next()
+})
+
+app.use((req, res, next)=>{
+    console.log('In another middleware')
+    next()
+})
+
+
+
+
 const server = http.createServer(app);
 
 app.listen(3000);
