1a995207b5cd99f271338ad93472ba4cd5ba6746
Venu <venukb6@gmail.com>
2024-06-03 17:56:59 +0530

    cleaned up the code using routers

--- starting_with_express_framework/get_started_with_express/app.js
+++ starting_with_express_framework/get_started_with_express/app.js
@@ -3,20 +3,14 @@ const bodyParser = require('body-parser')
 
 const app = express();
 
+const adminRoutes = require('./routes/admin')
+const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use('/add-product', (req, res, next)=>{
-    res.send('<form action="/product" method="POST"><input type="text" name="title" placeholder="product name"><input type="text" name="size" placeholder="product size"><button type="submit">Add Product</button></form>')
-})
+app.use(adminRoutes)
+app.use(shopRoutes)
 
-app.use('/product', (req, res, next)=>{
-    console.log(req.body)
-    res.redirect('/')
-})
-
-app.use('/', (req, res, next)=>{
-    res.send('<h1>Hello from express</h1>')
-})
 
 
 app.listen(3000);
