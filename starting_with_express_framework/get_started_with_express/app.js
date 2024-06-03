d130a4eedec0ddb6638e560771b1f33d7ae2df8f
Venu <venukb6@gmail.com>
2024-06-03 18:08:21 +0530

    made /admin and /shop routes as a seperate filter that call the admin.js and shop.js files respectively

--- starting_with_express_framework/get_started_with_express/app.js
+++ starting_with_express_framework/get_started_with_express/app.js
@@ -8,8 +8,8 @@ const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}))
 
-app.use(adminRoutes)
-app.use(shopRoutes)
+app.use('/admin', adminRoutes)
+app.use('/shop', shopRoutes)
 
 app.use((req, res, next)=>{
     res.status(404).send('<h1>Page Not Found</h1>')
