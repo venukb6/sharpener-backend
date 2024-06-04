e0a901cc1bfb475c417bc935c1034ea318debaa9
Venu <venukb6@gmail.com>
2024-06-04 18:14:34 +0530

    copied the project to new file to implement the contact us feature

---
+++ starting_with_express_framework/get_started_with_express_project_add_contact_us/routes/admin.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path')
+
+const rootDir = require('../utils/path')
+
+const router = express.Router()
+
+router.get('/add-product', (req, res, next)=>{
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'))
+})
+
+router.post('/product', (req, res, next)=>{
+    console.log(req.body)
+    res.redirect('/')
+})
+
+module.exports = router
+ 
