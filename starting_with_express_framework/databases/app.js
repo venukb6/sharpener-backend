0eeb725dd9ce07b3f7165d463db52a18e70a4d4f
Venu <venukb6@gmail.com>
2024-08-17 12:48:51 +0530

    Implemented cart system and orders system

--- starting_with_express_framework/databases/app.js
+++ starting_with_express_framework/databases/app.js
@@ -14,9 +14,12 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-const Poduct = require('./models/product');
 const User = require('./models/user');
 const Product = require('./models/product');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -37,8 +40,16 @@ app.use(errorController.get404);
 
 Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(Product, {through: OrderItem});
 
 sequelize
+    // .sync({ force: true })
     .sync()
     .then(result => {
         // console.log(result)
@@ -52,6 +63,10 @@ sequelize
     })
     .then(user => {
         // console.log(user);
+        // return user.createCart()
+        return 
+    })
+    .then(cart => {
         app.listen(3000)
     })
     .catch(err =>{
